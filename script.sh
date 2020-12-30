084e8b4329b585d0d477ae6bb86f189897a43223
Anita Nabieva <bruteforce1414@gmail.com>
2020-12-30 14:50:06 +0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-echo PWD:
-echo $PWD
-echo LS:
-echo $(ls -R)
-echo GO_ENV:
-echo $(go env)
-echo GO_GET:
+#echo PWD:
+#echo $PWD
+#echo LS:
+#echo $(ls -R)
+#echo GO_ENV:
+#echo $(go env)
+#echo GO_GET:
 #go get -v -t -d ./...
 #go test -v ./...
 #go clean ./...
 #echo CD_PLUGIN:
 #(cd plugin)
-echo GO_LIST_M:
-echo $(go list -m)
-echo GO_MOD_TIDY:
-$(go mod tidy)
-echo SET_ENV_GO111MODULE:
-set GO111MODULE=on
-echo BUILD:
+#echo GO_LIST_M:
+#echo $(go list -m)
+#echo GO_MOD_TIDY:
+#$(go mod tidy)
+#echo SET_ENV_GO111MODULE:
+#set GO111MODULE=on
+#echo BUILD:
 $(go build -buildmode=plugin main)
