12a63c9d08d4cf8a68a0145d9c45e97c073d3806
Anita Nabieva <bruteforce1414@gmail.com>
2020-12-30 13:39:42 +0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -11,6 +11,7 @@ go get -v -t -d ./...
 #go clean ./...
 #echo CD_PLUGIN:
 #(cd plugin)
+echo GO_LIST_M:
 echo $(go list -m)
 echo BUILD:
 $(go build -buildmode=plugin -o plugin.so ./plugin/main.go)
