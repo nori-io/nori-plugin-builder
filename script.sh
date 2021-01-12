29a18e4eb48619257119e9ed6855f29cee829f25
Anita Nabieva <bruteforce1414@gmail.com>
2021-01-12 11:51:19 +0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -17,4 +17,4 @@ go clean ./...
 echo GO_LIST_M:
 echo $(go list -m)
 echo BUILD:
-GO111module="on" go build --buildmode=plugin -o plugin.so ./plugin/main.go
+GO111module="on" CGO_ENABLED=0 go build --buildmode=plugin -o plugin.so ./plugin/main.go
